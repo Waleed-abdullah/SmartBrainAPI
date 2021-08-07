055d74aa0b72c1c859efac9c56e9290a38b079cb
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-07 16:46:25 +0500

    smartbrain api files

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '4232dd1bc64f4bb088accbbf66ff6a0b',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => res.json(entries[0]))
+    .catch((err) => res.status(400).json('unable to get entries'));
+};
+
+export { handleImage, handleApiCall };
---
+++ server.js
@@ -0,0 +1,51 @@
+import express, { json } from 'express';
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
+import knex from 'knex';
+import handleRegister from './controllers/register.js';
+import handleSignin from './controllers/signin.js';
+import handleProfileGet from './controllers/profile.js';
+import { handleImage, handleApiCall } from './controllers/image.js';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'waleed',
+    database: 'smartbrain',
+  },
+});
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json('success');
+});
+
+app.post('/signin', (req, res) => {
+  handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', (req, res) => {
+  handleRegister(req, res, db, bcrypt);
+});
+
+app.get('/profile/:id', (req, res) => {
+  handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  handleApiCall(req, res);
+});
+
+app.listen(8000, () => {
+  console.log('app is running');
+});
